d10ab56daca7b52fea2fc8fd88b9aa0bac207bdf
aetheryx <mhzy.ali@gmail.com>
2023-01-31 00:01:50 +0100

    add scripts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { writeFileSync } from 'node:fs';
 import { config } from './config/config.js';
 
-writeFileSync('./karabiner.json', JSON.stringify(config, null, '  '));
-console.log('done');
+const configRaw = JSON.stringify(config, null, '  ');
+process.stdout.write(configRaw);
